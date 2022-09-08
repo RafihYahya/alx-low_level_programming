efd24eb9738711eac808e6968a05563fa15ba310
Rafih Yahya <rafihyahya@gmail.com>
2022-09-08 15:31:16 -0700

    hhhhh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,7 @@ int main(void)
 	printf("Size of a double: %lu byte(s)\n", sizeof(double));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int);
-	printf("Size of a float: %lu byte(s)\n", sizeof(float);
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
 	return (0);
 }
