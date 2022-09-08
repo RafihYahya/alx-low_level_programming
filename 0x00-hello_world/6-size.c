fbc38b6ebc1c25d978943e2d7972cc85bdc8cfcc
Rafih Yahya <rafihyahya@gmail.com>
2022-09-08 15:29:23 -0700

    hhhhh

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a double: %lu byte(s)\n", sizeof(double));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int);
+	printf("Size of a float: %lu byte(s)\n", sizeof(float);
+	return (0);
+}
