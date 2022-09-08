77650944e67e077d066dd7706897c9300efcb852
Rafih Yahya <rafihyahya@gmail.com>
2022-09-08 15:11:25 -0700

    hhhhh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include "stdio.h"
 #include "stdlib.h"
 /* This is main */
-int main(void){
-
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
