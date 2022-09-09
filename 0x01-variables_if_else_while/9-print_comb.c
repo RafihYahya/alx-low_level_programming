bc48139dfd8c0a76afbdd65600ee916e70ac0f55
Rafih Yahya <rafihyahya@gmail.com>
2022-09-09 12:57:31 -0700

    hhhhh

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,14 +14,14 @@ int main(void)
 
 int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 8; i++)
 	{
 		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
 	}
 
-	putchar(',');
+	putchar(9 +'0');
 
 	return (0);
 }
