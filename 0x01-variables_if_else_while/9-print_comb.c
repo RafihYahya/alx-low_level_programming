cda33ad6636d3219ca281cd100e1a92e6819a409
Rafih Yahya <rafihyahya@gmail.com>
2022-09-09 12:52:52 -0700

    hhhhh

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int i;
+
+	for (i = 0;i <= 9; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' '0;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
