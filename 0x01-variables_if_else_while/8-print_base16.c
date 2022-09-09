73fa03eb2186a3207327a0747c89d1d9768cdfdc
Rafih Yahya <rafihyahya@gmail.com>
2022-09-09 12:43:55 -0700

    hhhhh

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n status; zero,p,n
+ *
+ * Return: Always 0(Success)
+ */
+
+
+int main(void)
+{
+
+int i;
+	for (i = 48; i <= 102; i++)
+		putchar(i);
+
+	printf("\n");
+
+	return (0);
+}
