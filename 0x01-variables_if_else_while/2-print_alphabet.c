942433d2b07087733108886fd475ceee23d9a049
Rafih Yahya <rafihyahya@gmail.com>
2022-09-09 08:07:45 -0700

    hhhhh

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+	
+	for(ch= 'a' ; ch <= 'z' ; ch++)
+		putchar(char);
+
+	return (0);
+}
