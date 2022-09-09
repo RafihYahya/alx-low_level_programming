36bb4dcedaae3e8756856fcda83dfe71ef383bbb
Rafih Yahya <rafihyahya@gmail.com>
2022-09-09 10:06:09 -0700

    hhhhh

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char ch;
+
+	for (ch = 'z' ; ch <= 'a' ; ch--)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
