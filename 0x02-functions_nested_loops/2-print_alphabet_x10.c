1b9959144837e9e8b31f81ca0e0ea4d75f4f8c9e
MaNomba <31630620+RafihYahya@users.noreply.github.com>
2022-09-13 18:36:34 +0200

    Life is always a war

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - use printchar to print lowercase number 10 times
+ */
+
+
+ void print_alphabet_x10(void)
+ {
+int j;
+    for (int j = 0; j <= 10 ; j++)
+    {
+    int i;
+	    for (i = 'a'; i <= 'z'; i++)
+		    _putchar(i);
+    }
+    _putchar('\n');
+ }
