1072a8f80309c9fbb26aefe67dff9d4103cfd46c
MaNomba <31630620+RafihYahya@users.noreply.github.com>
2022-09-13 17:42:43 +0200

    First test

---
+++ 0x02-functions_nested-loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n status; zero,p,n
+ *
+ * Return: Always 0(Success)
+ */
+
+
+int main(void)
+{
+	write (1,"_putchar",1)
+
+return (0);
+}
