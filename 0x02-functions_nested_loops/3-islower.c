612d4db5b042262e3ab71fb1d6dcceb3f31a82fe
MaNomba <31630620+RafihYahya@users.noreply.github.com>
2022-09-13 22:00:26 +0200

    dd

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -13,10 +13,10 @@ int _islower(int c)
 {
 int i;
 
-for (i = 'a'; i < 'z'; i++)
-{
-    if (c == i)
-        return (1);
-}
+    for (i = 'a'; i < 'z'; i++)
+    {
+if (c == i)
+    return (1);
+    }
 return (0);
 }
