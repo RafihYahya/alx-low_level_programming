996d909f7c6cad4eac956b47558158f917644146
MaNomba <31630620+RafihYahya@users.noreply.github.com>
2022-09-13 19:39:59 +0200

    ggg

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - function that check if  the alphabet is lowecase
+ *
+ *@c: checks input
+ *
+ *Return: return 1 if true, else 0
+ *
+*/
+
+int _isalpha(int a)
+{
+	if (c >= 97 && c <= 122 &&
+	    c >= 65 && c >= 90)
+		return (1);
+	return (0);
+}
