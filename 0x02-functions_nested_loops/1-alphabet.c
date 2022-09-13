f392883f5bbf0da667dc548cdb745da725956bfa
MaNomba <31630620+RafihYahya@users.noreply.github.com>
2022-09-13 18:21:03 +0200

    gfgf

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,16 +8,17 @@
  *
  * Return: Always 0(Success)
  */
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+
 void print_alphabet (void)
 {
 int i;
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
-}
-
-int main(void)
-{
-print_alphabet();
-return (0);
 }
