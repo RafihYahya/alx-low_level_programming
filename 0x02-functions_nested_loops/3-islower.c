a6fbf5aeca854db6df54563627230739654203db
MaNomba <31630620+RafihYahya@users.noreply.github.com>
2022-09-13 19:19:03 +0200

    dddd

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,18 +3,22 @@
 /**
  * _islower - function that check if  the alphabet is lowecase
  *
- *@c checks input
+ *@c: checks input
  *
  *Return: return 1 if true, else 0
+ *
 */
 
 int _islower(int c)
 {
 int i;
-    for (i = 'a'; i < 'z'; i++)
-    {
-        if (c == i)
-            return (1);
-    }
-return (0);    
+for (i = 'a'; i < 'z'; i++)
+{
+
+    if (c == i)
+        return (1);
+
+}
+
+return (0);
 }
