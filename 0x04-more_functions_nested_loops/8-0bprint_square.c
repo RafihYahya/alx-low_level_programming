4a8569838af8645701beed6ca17a134e73219880
MaNomba <31630620+RafihYahya@users.noreply.github.com>
2022-09-15 22:56:27 +0200

    dfff

---
+++ 0x04-more_functions_nested_loops/8-0bprint_square.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+ * print_square - print a square using the character #
+ *
+ * @size: is the size of the square
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_square(int size)
+{
+	int row, column;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 0; row <= size; row++)
+		{
+			for (column = 0; column <= size; column++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,21 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_square - print a square using the character #
- *
- * @size: is the size of the square
- *
- * Return: Always 0 (Success)
-*/
-
+ * print_square - prints a square, followed by a new line;
+ * @size: size of the square
+ */
 void print_square(int size)
 {
-	int row, column;
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
-		for (row = 0; row <= size; row++)
+		int i, j;
+
+		for (i = 0; i < size; i++)
 		{
-			for (column = 0; column <= size; column++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
