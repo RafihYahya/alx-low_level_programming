5d90f8450b7fbb6b9ced6dac4c621e87ae17ca06
MaNomba <31630620+RafihYahya@users.noreply.github.com>
2022-09-15 21:37:59 +0200

    fff

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,11 +11,17 @@
 void print_square(int size)
 {
 	int row, column;
-
-	for (row = 1; row <= size; ++row)
+	if (n <= 0)
 	{
-		for (column = 1; column <= size; ++column)
-			_putchar('#');
 		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; ++row)
+		{
+			for (column = 1; column <= size; ++column)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
