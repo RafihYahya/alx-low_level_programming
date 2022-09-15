50dd0a893cbf7b7a836b53cdceaff3fe9df34912
Rafih Yahya <rafihyahya@gmail.com>
2022-09-15 11:27:22 -0700

    hhhhh

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,14 @@
 
 /**
  * print_numbers - print num from 0 to 9
- *
- * no input
+ * 
  *
  * Return: no input
  */
 
 void print_numbers(void)
 {
-int i;
+	int i;
 	for (i = 48; i < 58; i++)
 		putchar(&i + '0');
 	putchar('\n');
