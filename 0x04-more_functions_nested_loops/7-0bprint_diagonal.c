17777b8d41f19d06ddcf20499b43bc86b1c66880
MaNomba <31630620+RafihYahya@users.noreply.github.com>
2022-09-15 22:57:13 +0200

    ggfg

---
+++ 0x04-more_functions_nested_loops/7-0bprint_diagonal.c
@@ -0,0 +1,30 @@
+#include"main.h"
+
+/**
+ * print_diagonal - print a diagonal line
+ *
+ * @n: is the number of times the \ character
+ *     should be printed
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_diagonal(int n)
+{
+	int i, space;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; ++i)
+		{
+			for (space = 1; space <= i; ++space)
+				_putchar(' ');
+			_putchar(92); /*is equal to '/' char*/
+			_putchar('\n');
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,29 +1,27 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_diagonal - print a diagonal line
- *
- * @n: is the number of times the \ character
- *     should be printed
- *
- * Return: Always 0 (Success)
-*/
-
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
+ */
 void print_diagonal(int n)
 {
-	int i, space;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
-		for (i = 1; i <= n; ++i)
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			for (space = 1; space <= i; ++space)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
 			_putchar('\n');
 		}
 	}
