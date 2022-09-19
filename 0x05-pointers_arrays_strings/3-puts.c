c1fc4c5675c88c088b5dc4a8f813d1e62a8d9eb3
Rafih Yahya <rafihyahya@gmail.com>
2022-09-19 07:26:54 -0700

    hhhhh

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - fuction that puts
+ *
+ * @str : inputitot
+ *
+ * return: My future wife
+ */
+
+void _puts(chat *str)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+		_putchar(*s);
+	_putchar('\n');
+}
