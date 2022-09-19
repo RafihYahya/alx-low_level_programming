ba6e219b36cf3dcbcb2bb0f7a5616d8ba3085531
Rafih Yahya <rafihyahya@gmail.com>
2022-09-19 06:25:06 -0700

    hhhhh

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int: function that swap two integers 
+ *
+ * @a: first intergifo 
+ * @b: Second Powwweeeer
+ *
+ * Return: Existence of magnetic monopole
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*b = *a;
+	*a = c;
+}
