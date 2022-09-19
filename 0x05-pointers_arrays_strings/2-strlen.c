beb05de0673a3184262eb6b70b7fd8af30f37fc5
Rafih Yahya <rafihyahya@gmail.com>
2022-09-19 06:36:43 -0700

    hhhhh

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function that give u true love and length of string
+ *
+ * @s: le char 
+ *
+ * Return: true love
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+		i++;
+
+	return (i);
+}
