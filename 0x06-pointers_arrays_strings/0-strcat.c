86c120f30c60ffa196a39fd25f3e096d8093faea
Rafih Yahya <rafihyahya@gmail.com>
2022-09-21 14:42:59 -0700

    hhhhh

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strcat - string concatination  super device
+ *
+ * @*dest: Our final destination
+ * @*src: what it might be ?
+ *
+ * Return : point toward it, the abyss that consume all of us
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i,j,k;
+
+	while (i != 2)
+	{
+		if (*(dest + j) == '\0')
+			i++;
+		if (i > 0 && i < 2)
+		{
+			*(dest + j) = *(src + k);
+			k++;
+		}
+		j++;
+	}
+	return *dest;
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
