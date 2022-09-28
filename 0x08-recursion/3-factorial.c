f8d855a0eecf96077a4932b4a93888ee18200aa2
Rafih Yahya <rafihyahya@gmail.com>
2022-09-28 13:46:40 -0700

    hhhhh

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - facto
+ *
+ * @n:mimo
+ *
+ * Return: nbi
+ */
+
+int factorial(int n)
+{
+	factorial(0) = 1;
+	if (n <= 0)
+		return (n*factorial(n - 1));
+	else
+		return (0);
