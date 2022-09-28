32de31fde41db1b48219f2dffd211c625bea1701
Rafih Yahya <rafihyahya@gmail.com>
2022-09-28 11:48:48 -0700

    hhhhh

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - puts but in  recursion
+ *
+ * @s : balabomba mino pini
+ *
+ * Return : your mom
+ */
+
+void _puts_recursion(char *s)
+{
+	if ( *s == '\0' )
+	{
+		_putchar(*s);
+		s = (s + 1);
+		_puts_recursion(s);
+	}
+	else
+	{
+		_putchar('\n');
+}
