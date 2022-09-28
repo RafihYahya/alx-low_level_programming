cc311f7685e276020cc9ff26036926cab1adf04e
Rafih Yahya <rafihyahya@gmail.com>
2022-09-28 16:56:35 -0700

    hhhhh

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - miawtabaw
+ *
+ * @n : lolita m7amra
+ *
+ * Return: lah ijib
+ */
+
+int cstn(int n)
+{
+	int *p;
+	p = &n;
+	*p = *p + 1;
+	return (n + *p);
+}
+
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	if (n == 0)
+		return (-1);
+	if (n < 0)
+		return (-1);
+	i = n*n;
+	if (i == cstn())
+		return (n);
+	_sqrt_recursion(n - 1);
+}	
