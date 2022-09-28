d6fc63be674cbe30048aea0d73dc270dca89ebed
Rafih Yahya <rafihyahya@gmail.com>
2022-09-28 13:20:35 -0700

    hhhhh

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -13,11 +13,7 @@ void _print_rev_recursion(char *s)
 
 	if (*s != '\0')
 	{
-		i++;
 		_print_rev_recursion(s + 1);
-		_print_rev_recursion(s - 1);
+		_putchar(*s);
 	}
-	else
-	{
-		_putchar('\n');
-	}
+}
